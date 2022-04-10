eb52e00da3cc71bd7cc382b69f70d8b672f351e4
Matt Moore <Matt.M.Moore13@outlook.com>
2022-04-10 00:31:34 -0400

    began outfitting the custom user model

--- ia/user/models.py
+++ ia/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, firstName, lastName, phoneNumber, email, password=None, **kwargs):
+        """Create and return a `User` with a first name, last name, phone number, email, and password."""
+        if firstName is None:
+            raise TypeError('Users must have a first name')
+        if lastName is None:
+            raise TypeError('Users must have a last name')
+        
